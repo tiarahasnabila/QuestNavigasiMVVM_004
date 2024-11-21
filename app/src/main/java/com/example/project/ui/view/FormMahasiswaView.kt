75a2b0b4454a3ac3247600905ecd674a3fa3a4fb
tiarahasnabila <tiarahasnabila@gmail.com>
2024-11-21 16:50:51 +0700

    fungsi untuk membuat tampilan formulir mahasiswa, menggunakan mutablestateof untuk menyimpan nilai input penggguna

---
+++ app/src/main/java/com/example/project/ui/view/FormMahasiswaView.kt
@@ -0,0 +1,50 @@
+package com.example.project.ui.view
+
+
+import android.provider.MediaStore.Audio.Radio
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.GrammaticalInflectionManagerCompat.GrammaticalGender
+
+@Composable
+fun FormMahasiswaView(modifier : Modifier = Modifier,
+                      listGender: List<String>,
+                      onSubmitClick: (MutableList<String>) -> Unit
+){
+
+    var nama by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("")}
+    var noTelp by remember { mutableStateOf("")}
+    var alamat by remember { mutableStateOf("")}
+    var memilihJk by remember { mutableStateOf("")}
+
+
+    val listData: MutableList<String> = mutableListOf(
+        nama,memilihJk,alamat
+    )
+
+
+
+
